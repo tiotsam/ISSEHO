5bca1a4c045aa7ceda39ea2275a5b67ca8154e76
Sam <tiotsam@gmail.com>
2021-12-17 15:30:15 +0100

    maj fixtures

--- issehoAPI/src/DataFixtures/CoursFixtures.php
+++ issehoAPI/src/DataFixtures/CoursFixtures.php
@@ -27,28 +27,37 @@ class CoursFixtures extends Fixture implements DependentFixtureInterface
     {
         // $product = new Product();
         // $manager->persist($product);
+        $matières = array('Mathématiques','Français','Echec','Anglais','Informatique','Physique','Chimie');
+        $niveaux = array('CP','CE1','CE2','CM1','CM2','6ème','5ème','4ème','3ème','2nde','1ère','terminale','BEP','CAP','BAC PRO');
 
-        for($i=0 ; $i < 30; $i++){
-            $cour = new Cours;
-            $auteur = new User;
-            $mat = new Matieres;
-            $level = new Niveau;
-            $mat = $this->matieresRepository->findOneBy(array('id'=>random_int(1,7)));
-            $level = $this->niveauRepository->findOneBy(array('id'=>random_int(1,15)));
-            $auteur = $this->userRepository->findOneBy(array('email'=>'prof'.random_int(0,9).'@gmail.com'));
-            $dateD = new \DateTime(random_int(1,12).'/'.random_int(1,28).'/2021');
-            $cour->setDateDebut($dateD)
-            ->setDateFin($dateD->add(new DateInterval('PT2H')))
-            ->setAuteur($auteur)
-            ->setMatieres($mat)
-            ->setNiveau($level);
-            for ($i=0; $i < random_int(1,5); $i++) {
-                $eleve = new User;
-                $eleve = $this->userRepository->findOneBy(array('email'=>'enfant'.random_int(0,9).'@gmail.com'));
-                $cour->addParticipant($eleve);
-            }
-            $manager->persist($cour);
-        }
+        // $user = $this->userRepository->findOneBy(array('email'=>'toto@gmail.com'));
+        
+        $auteur = new User;
+        // $auteur = $this->userRepository->findOneBy(array('email'=>'prof'.random_int(0,9).'@gmail.com'));
+        $auteur = $this->userRepository->findOneBy(array('email'=>'prof0@gmail.com'));
+        var_dump($auteur);
+
+        // for($i=0 ; $i < 30; $i++){
+        //     $cour = new Cours;
+        //     $mat = new Matieres;
+        //     $level = new Niveau;
+
+        //     $mat = $this->matieresRepository->findOneBy(array('nom'=>$matières[random_int(0,sizeof($matières)-1)]));
+        //     $level = $this->niveauRepository->findOneBy(array('nom'=>$niveaux[random_int(0,sizeof($niveaux)-1)]));
+
+        //     // $dateD = new \DateTime(random_int(1,12).'/'.random_int(1,28).'/2021');
+        //     // $cour->setDateDebut($dateD)
+        //     // ->setDateFin($dateD->add(new DateInterval('PT2H')))
+        //     // ->setAuteur($auteur)
+        //     // ->setMatieres($mat)
+        //     // ->setNiveau($level);
+        //     // for ($i=0; $i < random_int(1,5); $i++) {
+        //     //     $eleve = new User;
+        //     //     $eleve = $this->userRepository->findOneBy(array('email'=>'enfant'.random_int(0,9).'@gmail.com'));
+        //     //     $cour->addParticipant($eleve);
+        //     // }
+        //     // $manager->persist($cour);
+        // }
 
 
         $manager->flush();
@@ -56,6 +65,6 @@ class CoursFixtures extends Fixture implements DependentFixtureInterface
 
     public function getDependencies()
     {
-        return [UserFixtures::class , MatièresFixtures::class , NiveauFixtures::class];
+        return [UserFixtures::class,MatièresFixtures::class,NiveauFixtures::class];
     }
 }
