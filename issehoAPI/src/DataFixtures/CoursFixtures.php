9c4ddbdf5ddcb815a3187091e65dbcf1945b44ab
Sam <tiotsam@gmail.com>
2021-12-17 17:57:47 +0100

    fix userRepository

--- issehoAPI/src/DataFixtures/CoursFixtures.php
+++ issehoAPI/src/DataFixtures/CoursFixtures.php
@@ -32,35 +32,34 @@ class CoursFixtures extends Fixture implements DependentFixtureInterface
 
         // $user = $this->userRepository->findOneBy(array('email'=>'toto@gmail.com'));
         
-        $auteur = new User;
         // $auteur = $this->userRepository->findOneBy(array('email'=>'prof'.random_int(0,9).'@gmail.com'));
-        $auteur = $this->userRepository->findOneBy(array('email'=>'prof0@gmail.com'));
-        var_dump($auteur);
-
+        
         // for($i=0 ; $i < 30; $i++){
         //     $cour = new Cours;
         //     $mat = new Matieres;
         //     $level = new Niveau;
-
+        //     $auteur = new User;
+        
         //     $mat = $this->matieresRepository->findOneBy(array('nom'=>$matières[random_int(0,sizeof($matières)-1)]));
         //     $level = $this->niveauRepository->findOneBy(array('nom'=>$niveaux[random_int(0,sizeof($niveaux)-1)]));
-
-        //     // $dateD = new \DateTime(random_int(1,12).'/'.random_int(1,28).'/2021');
-        //     // $cour->setDateDebut($dateD)
-        //     // ->setDateFin($dateD->add(new DateInterval('PT2H')))
-        //     // ->setAuteur($auteur)
-        //     // ->setMatieres($mat)
-        //     // ->setNiveau($level);
-        //     // for ($i=0; $i < random_int(1,5); $i++) {
-        //     //     $eleve = new User;
-        //     //     $eleve = $this->userRepository->findOneBy(array('email'=>'enfant'.random_int(0,9).'@gmail.com'));
-        //     //     $cour->addParticipant($eleve);
-        //     // }
-        //     // $manager->persist($cour);
+        //     $auteur = $this->userRepository->findOneBy(array('email'=>'prof'.random_int(0,9).'@gmail.com'));
+            
+        //     $dateD = new \DateTime(random_int(1,12).'/'.random_int(1,28).'/2021');
+        //     $cour->setDateDebut($dateD)
+        //     ->setDateFin($dateD->add(new DateInterval('PT2H')))
+        //     ->setAuteur($auteur)
+        //     ->setMatieres($mat)
+        //     ->setNiveau($level);
+        //     for ($i=0; $i < random_int(1,5); $i++) {
+        //         $eleve = new User;
+        //         $eleve = $this->userRepository->findOneBy(array('email'=>'enfant'.random_int(0,9).'@gmail.com'));
+        //         $cour->addParticipant($eleve);
+        //     }
+        //     $manager->persist($cour);
         // }
 
 
-        $manager->flush();
+        // $manager->flush();
     }
 
     public function getDependencies()
