2cc4d61113b08cb4f038cb486e616cf46396dc19
Sam <tiotsam@gmail.com>
2021-12-19 16:14:13 +0100

    maj cours fixtures mailauto fixtures fonctionnelles

--- issehoAPI/src/DataFixtures/CoursFixtures.php
+++ issehoAPI/src/DataFixtures/CoursFixtures.php
@@ -31,39 +31,38 @@ class CoursFixtures extends Fixture implements DependentFixtureInterface
         
         $matières = array('Mathématiques','Français','Echec','Anglais','Informatique','Physique','Chimie');
         $niveaux = array('CP','CE1','CE2','CM1','CM2','6ème','5ème','4ème','3ème','2nde','1ère','terminale','BEP','CAP','BAC PRO');
-
-        // $user = $this->userRepository->findOneBy(array('email'=>'toto@gmail.com'));
         
-        // for($i=0 ; $i < 30; $i++){
-        //     $cour = new Cours;
-        //     $mat = new Matieres;
-        //     $level = new Niveau;
-        //     $auteur = new User;
+        for($i=0 ; $i < 30; $i++){
+            $cour = new Cours;
+            $mat = new Matieres;
+            $level = new Niveau;
+            $auteur = new User;
         
-        //     $mat = $this->matieresRepository->findOneBy(array('nom'=>$matières[random_int(0,sizeof($matières)-1)]));
-        //     $level = $this->niveauRepository->findOneBy(array('nom'=>$niveaux[random_int(0,sizeof($niveaux)-1)]));
-        //     $auteur = $this->userRepository->findOneBy(array('email'=>'prof'.random_int(0,9).'@gmail.com'));
+            $mat = $this->matieresRepository->findOneBy(array('nom'=>$matières[random_int(0,sizeof($matières)-1)]));
+            $level = $this->niveauRepository->findOneBy(array('nom'=>$niveaux[random_int(0,sizeof($niveaux)-1)]));
+            $auteur = $this->userRepository->findOneBy(array('email'=>'prof'.random_int(0,9).'@gmail.com'));
             
-        //     $dateD = new \DateTime(random_int(1,12).'/'.random_int(1,28).'/2021');
-        //     $cour->setDateDebut($dateD)
-        //     ->setDateFin($dateD->add(new DateInterval('PT2H')))
-        //     ->setAuteur($auteur)
-        //     ->setMatieres($mat)
-        //     ->setNiveau($level);
-        //     for ($i=0; $i < random_int(1,5); $i++) {
-        //         $eleve = new User;
-        //         $eleve = $this->userRepository->findOneBy(array('email'=>'enfant'.random_int(0,9).'@gmail.com'));
-        //         $cour->addParticipant($eleve);
-        //     }
-        //     $manager->persist($cour);
-        // }
+            $dateD = new \DateTime(random_int(1,12).'/'.random_int(1,28).'/2021');
+            $cour->setDateDebut($dateD)
+            ->setDateFin($dateD->add(new DateInterval('PT2H')))
+            ->setAuteur($auteur)
+            ->setMatieres($mat)
+            ->setNiveau($level);
+            for ($j=0; $j < random_int(1,5); $j++) {
+                $eleve = new User;
+                $eleve = $this->userRepository->findOneBy(array('email'=>'enfant'.random_int(0,9).'@gmail.com'));
+                $cour->addParticipant($eleve);
+            }
+            $manager->persist($cour);
+            // var_dump("cour".$i."enregistré !");
+        }
 
 
-        // $manager->flush();
+        $manager->flush();
     }
 
     public function getDependencies()
     {
-        return [UserFixtures::class,MatièresFixtures::class,NiveauFixtures::class];
+        return [UserFixtures::class,InfosFixtures::class,MatièresFixtures::class,NiveauFixtures::class];
     }
 }
--- issehoAPI/src/DataFixtures/InfosFixtures.php
+++ issehoAPI/src/DataFixtures/InfosFixtures.php
@@ -16,7 +16,7 @@ class InfosFixtures extends Fixture implements DependentFixtureInterface
 
     public function __construct(UserRepository $userRepository, InfosRepository $infosRepository)
     {
-        
+
         $this->userRepository = $userRepository;
         $this->infoRepository = $infosRepository;
     }
@@ -27,85 +27,91 @@ class InfosFixtures extends Fixture implements DependentFixtureInterface
         // $product = new Product();
         // $manager->persist($product);
 
-        $nom = array ('martin','bernard','thomas','petit','robert','richard','durand','dubois','moreau','laurent','simon','michel','lefevre','leroy','roux','david','morel','bertrand','fournier','girard');
-        $prenom = array ('emma','liam','alice','sophia','nathan','Florence','william','Logan','charlie','léo','olivia','thomas','mia','amélia','félix','noah','léa','jacob','édouard','charlotte');
-        $ville = array('abilly','paris','marseille','lyon','toulouse','nice','rennes','reims','toulon','saint-étienne','le havre','grenoble','dijon','angers','villeurbanne','Saint-denis');
+        $nom = array('martin', 'bernard', 'thomas', 'petit', 'robert', 'richard', 'durand', 'dubois', 'moreau', 'laurent', 'simon', 'michel', 'lefevre', 'leroy', 'roux', 'david', 'morel', 'bertrand', 'fournier', 'girard');
+        $prenom = array('emma', 'liam', 'alice', 'sophia', 'nathan', 'Florence', 'william', 'Logan', 'charlie', 'léo', 'olivia', 'thomas', 'mia', 'amélia', 'félix', 'noah', 'léa', 'jacob', 'édouard', 'charlotte');
+        $ville = array('abilly', 'paris', 'marseille', 'lyon', 'toulouse', 'nice', 'rennes', 'reims', 'toulon', 'saint-étienne', 'le havre', 'grenoble', 'dijon', 'angers', 'villeurbanne', 'Saint-denis');
         $imageuser = array('https://us.123rf.com/450wm/kritchanut/kritchanut1401/kritchanut140100054/25251050-photo-de-profil-d-affaires-de-l-avatar.jpg?ver=6');
-        $imageenfant = array ('https://us.123rf.com/450wm/kritchanut/kritchanut1401/kritchanut140100054/25251050-photo-de-profil-d-affaires-de-l-avatar.jpg?ver=6');
+        $imageenfant = array('https://us.123rf.com/450wm/kritchanut/kritchanut1401/kritchanut140100054/25251050-photo-de-profil-d-affaires-de-l-avatar.jpg?ver=6');
 
 
-        for($i=0 ; $i < 10; $i++){
-        
+        for ($i = 0; $i < 10; $i++) {
 
-        $user = new User;
-        $info = new Infos;
-        $user = $this->userRepository->findOneBy(array("email"=>'prof1@gmail.com'));
-        $birthdate = new \DateTime(random_int(1,12).'/'.random_int(1,28).'/'.random_int(1960,2000));
-        $dateinscription = new \DateTime('12/'.random_int(1,28).'/2020');
-        $info->setNom($nom[random_int(0,sizeof($nom)-1)])
-        ->setPrenom($prenom[random_int(0,sizeof($prenom)-1)])
-        ->setDepartement(random_int(1,95))
-        ->setVille($ville[random_int(0,sizeof($ville)-1)])
-        ->setBirthDate($birthdate)
-        ->setImageUser($imageuser[random_int(0,sizeof($imageuser)-1)])
-        ->setDateInscription($dateinscription)
-        ->setUser($user);
 
-        
+            $user = new User;
+            $info = new Infos;
+            $user = $this->userRepository->findOneBy(array("email" => 'prof'.$i.'@gmail.com'));
+            $birthdate = new \DateTime(random_int(1, 12) . '/' . random_int(1, 28) . '/' . random_int(1960, 2000));
+            $dateinscription = new \DateTime('12/' . random_int(1, 28) . '/2020');
+            $info->setNom($nom[random_int(0, sizeof($nom) - 1)])
+                ->setPrenom($prenom[random_int(0, sizeof($prenom) - 1)])
+                ->setDepartement(random_int(1, 95))
+                ->setVille($ville[random_int(0, sizeof($ville) - 1)])
+                ->setBirthDate($birthdate)
+                ->setImageUser($imageuser[random_int(0, sizeof($imageuser) - 1)])
+                ->setDateInscription($dateinscription)
+                ->setUser($user);
+
+
 
             $manager->persist($info);
+            $user->setInfos($info);
+            $manager->persist($user);
         }
 
-        for($i=0 ; $i < 10; $i++){
-        
+        for ($i = 0; $i < 10; $i++) {
+
 
             $user = new User;
             $info = new Infos;
             $enfant = new User;
-            $enfant = $this->userRepository->findOneBy(array("email"=>'enfant1@gmail.com'));
-            $user = $this->userRepository->findOneBy(array("email"=>'parent1@gmail.com'));
-            $birthdate = new \DateTime(random_int(1,12).'/'.random_int(1,28).'/'.random_int(1960,2000));
-            $dateinscription = new \DateTime('12/'.random_int(1,28).'/2020');
-            $info->setNom($nom[random_int(0,sizeof($nom)-1)])
-            ->setPrenom($prenom[random_int(0,sizeof($prenom)-1)])
-            ->setDepartement(random_int(1,95))
-            ->setVille($ville[random_int(0,sizeof($ville)-1)])
-            ->setBirthDate($birthdate)
-            ->setImageUser($imageuser[random_int(0,sizeof($imageuser)-1)])
-            ->setDateInscription($dateinscription)
-            ->addEnfant($enfant)
-            ->setUser($user);
-            
+            $enfant = $this->userRepository->findOneBy(array("email" => 'enfant'.$i.'@gmail.com'));
+            $user = $this->userRepository->findOneBy(array("email" => 'parent'.$i.'@gmail.com'));
+            $birthdate = new \DateTime(random_int(1, 12) . '/' . random_int(1, 28) . '/' . random_int(1960, 2000));
+            $dateinscription = new \DateTime('12/' . random_int(1, 28) . '/2020');
+            $info->setNom($nom[random_int(0, sizeof($nom) - 1)])
+                ->setPrenom($prenom[random_int(0, sizeof($prenom) - 1)])
+                ->setDepartement(random_int(1, 95))
+                ->setVille($ville[random_int(0, sizeof($ville) - 1)])
+                ->setBirthDate($birthdate)
+                ->setImageUser($imageuser[random_int(0, sizeof($imageuser) - 1)])
+                ->setDateInscription($dateinscription)
+                ->addEnfant($enfant)
+                ->setUser($user);
 
-    
-            
-    
-                $manager->persist($info);
-            }
 
-             
 
-                    for($i=0 ; $i < 10; $i++){
-        
 
-                        $user = new User;
-                        $info = new Infos;
-                        $user = $this->userRepository->findOneBy(array("email"=>'enfant2@gmail.com'));
-                        $birthdate = new \DateTime(random_int(1,12).'/'.random_int(1,28).'/'.random_int(2005,2015));
-                        $dateinscription = new \DateTime('12/'.random_int(1,28).'/2020');
-                        $info->setNom($nom[random_int(0,sizeof($nom)-1)])
-                        ->setPrenom($prenom[random_int(0,sizeof($prenom)-1)])
-                        ->setDepartement(random_int(1,95))
-                        ->setVille($ville[random_int(0,sizeof($ville)-1)])
-                        ->setBirthDate($birthdate)
-                        ->setImageUser($imageenfant[random_int(0,sizeof($imageenfant)-1)])
-                        ->setDateInscription($dateinscription)
-                        ->setUser($user);
-                
-                        
-                
-                            $manager->persist($info);
-                        }
+
+            $manager->persist($info);
+            $user->setInfos($info);
+            $manager->persist($user);
+        }
+
+
+
+        for ($i = 0; $i < 10; $i++) {
+
+
+            $user = new User;
+            $info = new Infos;
+            $user = $this->userRepository->findOneBy(array("email" => 'enfant'.$i.'@gmail.com'));
+            $birthdate = new \DateTime(random_int(1, 12) . '/' . random_int(1, 28) . '/' . random_int(2005, 2015));
+            $dateinscription = new \DateTime('12/' . random_int(1, 28) . '/2020');
+            $info->setNom($nom[random_int(0, sizeof($nom) - 1)])
+                ->setPrenom($prenom[random_int(0, sizeof($prenom) - 1)])
+                ->setDepartement(random_int(1, 95))
+                ->setVille($ville[random_int(0, sizeof($ville) - 1)])
+                ->setBirthDate($birthdate)
+                ->setImageUser($imageenfant[random_int(0, sizeof($imageenfant) - 1)])
+                ->setDateInscription($dateinscription)
+                ->setUser($user);
+
+
+
+            $manager->persist($info);
+            $user->setInfos($info);
+            $manager->persist($user);
+        }
 
 
 
