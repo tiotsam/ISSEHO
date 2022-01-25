febb7ad1239f6e09db05b8ba4af9ebf8a37b60b2
zantess <zenati.prodesign@yahoo.com>
2022-01-25 11:51:43 +0100

    fixe footer

--- isseho/src/Componente/Footer.js
+++ isseho/src/Componente/Footer.js
@@ -1,94 +1,62 @@
 import React from 'react';
 import '../Style/Footer.css';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { IoLogoFacebook, IoLogoYoutube, IoLogoInstagram } from "react-icons/io5";
 
 function Footer() {
-    return (
-      <div className='footer-container'>
-        <section className='footer-subscription'>
-    
-          <p className='footer-subscription-text'>
-            Nous contacter
-          </p>
-          <div className='input-areas'>
-            <form>
-            
-              <input
-                className='footer-input'
-                name='email'
-                type='email'
-                placeholder='Votre Email'
-              />
-            </form>
-          </div>
-        </section>
-        <div class='footer-links'>
-          <div className='footer-link-wrapper'>
-            <div class='footer-link-items'>
-              <h4>Qui somme nous ?</h4>
-              <Link to='/'>Notre Société</Link>
-              <Link to='/'>Nos Projets</Link>
-              <Link to='/'>Notre Vision</Link>
-          
-            </div>
-            <div class='footer-link-items'>
-              <h4>Contact</h4>
-              <Link to='/'>Email: Contact@isseho.com</Link>
-              <Link to='/'>Téléphone: 01 58 95 65 20</Link>
-            </div>
-            </div>
+  return (
+    <div className='footer-container'>
+      <section className='footer-subscription'>
+
+        <p className='footer-subscription-text'>
+          Nous contacter
+        </p>
+        <div className='input-areas'>
+
+          <form>
+            <input
+              className='footer-input'
+              name='email'
+              type='email'
+              placeholder='Votre Email'
+            />
+          </form>
         </div>
-        <section class='social-media'>
-          <div class='social-media-wrap'>
-            <div class='footer-logo'>
-              <Link to='/' className='social-logo'>
-                Isseho
-              </Link>
-            </div>
-            <small class='website-rights'>ISSEHO © 2021</small>
-            <div class='social-icons'>
-              <Link
-                class='social-icon-link facebook'
-                to='/'
-                target='_blank'
-              >
-                
-              </Link>
-              <Link
-                class='social-icon-link instagram'
-                to='/'
-                target='_blank'
-              >
-                
-              </Link>
-              <Link
-                class='social-icon-link youtube'
-                to='/'
-                target='_blank'
-                aria-label='Youtube'
-              >
-                
-              </Link>
-              <Link
-                class='social-icon-link twitter'
-                to='/'
-                target='_blank'
-                aria-label='Twitter'
-              >
-                
-              </Link>
-              <Link
-                class='social-icon-link twitter'
-                to='/'
-                target='_blank'
-                aria-label='LinkedIn'
-              >
-              </Link>
-            </div>
+      </section>
+      <div class='footer-links'>
+        <div className='footer-link-wrapper'>
+          <div class='footer-link-items'>
+            <h4>Qui somme nous ?</h4>
+            <Link to='/'>Notre Société</Link>
+            <Link to='/'>Notre Vision</Link>
+
           </div>
-        </section>
+          <div class='footer-link-items'>
+            <h4>Contact</h4>
+            <Link to='/'>Email: Contact@isseho.com</Link>
+            <Link to='/'>Tél: 01 58 95 65 20</Link>
+          </div>
+        </div>
       </div>
-    );
-  }
-  
-  export default Footer;
+      <div className='footer_elem'></div>
+      <div className='container_footer-mediai'>
+      </div>
+       
+      <section class='social-media'>
+        <div class='social-media-wrap'>
+          <div class='footer-logo'>
+          </div>
+          <small class='website-rights'>
+          <IoLogoFacebook />
+            <IoLogoYoutube />
+            <IoLogoInstagram/>
+          </small>
+          <div class='social-icons'>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Footer;
