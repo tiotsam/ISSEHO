61b3f7673a9cde32d9993192c8c1438f2b491b95
zantess <zenati.prodesign@yahoo.com>
2022-01-26 14:00:07 +0100

    modifiaction css

--- isseho/src/Componente/Footer.js
+++ isseho/src/Componente/Footer.js
@@ -2,26 +2,34 @@ import React from 'react';
 import '../Style/Footer.css';
 import { Link } from 'react-router-dom';
 import { IoLogoFacebook, IoLogoYoutube, IoLogoInstagram } from "react-icons/io5";
+import Popup from 'reactjs-popup';
+
+
 
 function Footer() {
   return (
+
+
     <div className='footer-container'>
       <section className='footer-subscription'>
-
         <p className='footer-subscription-text'>
-          Nous contacter
+          NOUS CONTACTER
         </p>
-        <div className='input-areas'>
-
-          <form>
-            <input
-              className='footer-input'
-              name='email'
-              type='email'
-              placeholder='Votre Email'
-            />
-          </form>
-        </div>
+        <Popup trigger={<button className='trigger_footer'> c'est ici </button>} position="top center">
+          <div className='popup_footer'>
+            <div className='wrapper_popup'>
+            <div className='container_popup'>
+              <h5 className='popup_H5'>Adress email</h5>
+              <input className='input_area' placeholder='votre adress-email'></input>
+              <h5 className='popup_H5'>Objet</h5>
+              <input placeholder='votre sujet'></input>
+              <h5 className='popup_H5'>Message</h5>
+              <textarea className='area_popup'  placeholder='votre message' ></textarea>
+              <button className='button_popup'>Envoyer</button>
+              </div>
+            </div>
+          </div>
+        </Popup>
       </section>
       <div class='footer-links'>
         <div className='footer-link-wrapper'>
@@ -41,15 +49,15 @@ function Footer() {
       <div className='footer_elem'></div>
       <div className='container_footer-mediai'>
       </div>
-       
+
       <section class='social-media'>
         <div class='social-media-wrap'>
           <div class='footer-logo'>
           </div>
           <small class='website-rights'>
-          <IoLogoFacebook />
+            <IoLogoFacebook />
             <IoLogoYoutube />
-            <IoLogoInstagram/>
+            <IoLogoInstagram />
           </small>
           <div class='social-icons'>
           </div>
