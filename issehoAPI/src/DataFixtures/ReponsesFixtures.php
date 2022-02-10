14fe0a14ab362144fb17e890a0aa75a1f4abbb69
Sam <tiotsam@gmail.com>
2022-02-10 12:27:50 +0100

    fix reponses fixtures

--- issehoAPI/src/DataFixtures/ReponsesFixtures.php
+++ issehoAPI/src/DataFixtures/ReponsesFixtures.php
@@ -29,30 +29,49 @@ class ReponsesFixtures extends Fixture implements DependentFixtureInterface
 
         $questProfs = $this->questionsRepository->findBy(array('cible'=>'Prof'));
         $questParents = $this->questionsRepository->findBy(array('cible'=>'Parent'));
-        
+
+
         $reponsesProf = array("J'adore transmettre mon savoir à ceux qui en ont besoin.","Il faut bien manger.","J'adore les enfants.",'Ecole','Collège','Lycée','Université','Bouche à oreilles','recherche sur internet','la réponse D');
         $reponsesParent = array('Bouche à oreilles.','Recherche sur internet','vu à la télé',"J'ai toujours aimé les enfants.","Besoin de main d'oeuvre","Pas fait exprès","Mon enfant a du mal à suivre à l'école, il a besoin d'aide","Mon enfant s'en sort mais veut s'améliorer","Aider moi mon gosse est nul !");
 
-
         for ($i=0; $i < 10; $i++) { 
-            $repProf = new Reponses;
             $prof = new User;
             $prof = $this->userRepository->findOneBy(['email' => 'prof'.$i.'@gmail.com']);
-
+            
+            
             foreach ($questProfs as $key => $question) {
-                $repProf->setLabel($reponsesProf[random_int(0,sizeof($reponsesProf)-1)])
+                $repProf = new Reponses;
+                if($key == 0){
+                   $labelRep = $reponsesProf[random_int(0,2)];
+                }
+                if($key == 1){
+                    $labelRep = $reponsesProf[random_int(3,6)];
+                }
+                if($key == 2){
+                    $labelRep = $reponsesProf[random_int(7,9)];
+                }
+                $repProf->setLabel($labelRep)
                 ->setQuestion($question)
                 ->setUser($prof);
                 $manager->persist($repProf);
             }
 
-            $repParent = new Reponses;
             $parent = new User;
             $parent = $this->userRepository->findOneBy(['email' => 'parent'.$i.'@gmail.com']);
-
-            foreach ($questParents as $key => $question2) {
-                $repParent->setLabel($reponsesParent[random_int(0,sizeof($reponsesParent)-1)])
-                ->setQuestion($question2)
+            
+            foreach ($questParents as $key2 => $quest) {
+                $repParent = new Reponses;
+                if($key2 == 0){
+                    $labelRep2 = $reponsesParent[random_int(0,2)];
+                 }
+                 if($key2 == 1){
+                     $labelRep2 = $reponsesParent[random_int(3,5)];
+                 }
+                 if($key2 == 2){
+                     $labelRep2 = $reponsesParent[random_int(6,8)];
+                 }
+                $repParent->setLabel($labelRep2)
+                ->setQuestion($quest)
                 ->setUser($parent);
                 $manager->persist($repParent);
             }
